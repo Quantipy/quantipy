675b8965529b4e41f3465362dd4da9400d811f3d
James Griffiths <jamesrkg@users.noreply.github.com>
2015-08-04 14:54:23 -0400

    Added docstring and comments to engine.get_report() and edited for PEP8.

--- core/weights/weight_engine.py
+++ core/weights/weight_engine.py
@@ -41,47 +41,100 @@ class WeightEngine:
                     "\n constructor. If your meta is serialized please load it first."
                     )
             self._meta = meta
-
+            
     def get_report(self):
+        """
+        Return a DataFrame summarising results of the calculated weights.
 
+        Used to return the metrics commonly used to measure the
+        effectiveness of the calculated weights. Metrics returned
+        (below) will be broken down by weight variable and weight
+        filter.
+
+        - Total: unweighted
+        - Total: weighted
+        - Weighting efficiency
+        - Iterations required
+        - Mean weight factor
+        - Minimum weight factor
+        - Maximum weight factor
+        - Weight factor ratio
+
+        Parameters
+        ----------
+        self : self
+
+        Returns
+        -------
+        None
+        """
+
+        # List to store each report series for later concatenation
         reports = []
+
         for scheme_name in sorted(self.schemes.keys()):
             scheme = self.schemes[scheme_name]['scheme']
             data = self.dataframe(scheme_name)
-            
+
             for group_name in sorted(scheme.groups.keys()):
                 report = pd.Series([])
                 weight_col = 'weights_{0}'.format(scheme_name)
                 group = scheme.groups[group_name]
-                filter_def = group['filters']      
+                filter_def = group['filters']
+
                 report['Weight variable'] = weight_col
                 report['Weight group'] = group_name
 
                 if filter_def is None:
                     report['Weight filter'] = 'None'
+                    # This is a trick to keep rows that are not NaN
+                    # since Nan is never equal to NaN.
                     filter_def = '{0}=={0}'.format(weight_col)
                 else:
                     report['Weight filter'] = filter_def
-                
+
                 filtered_data = data.query(filter_def)
                 weight_factors = filtered_data[weight_col]
 
                 weight_count = weight_factors.count()
                 weight_sum = weight_factors.sum()
 
+                # The weighting efficiency is normally calculated by
+                # first dividing the squared sum of the weights by the
+                # number of respondents, then dividing that by the sum
+                # of the squared weights, however, in order to account
+                # for controlling the total weighted base the
+                # calculation here is changed to multiplying the sum of
+                # the weights divided by the number of respondents by
+                # the sum of the weights divided by the sum of the
+                # squared weights. If you don't make this change the
+                # weighting efficiency for schemes with controlled
+                # bases will be incorrect.
+                efficiency = (
+                    (weight_sum / weight_count) * (
+                        weight_sum / weight_factors.pow(2).sum()
+                    )
+                ) * 100
+                mean = filtered_data[weight_col].mean()
+                minimum = filtered_data[weight_col].min()
+                maximum = filtered_data[weight_col].max()
+
                 report['Total: unweighted'] = weight_count
-                report['Total: weighted'] = weight_sum                
-                efficiency = ((weight_sum / weight_count) * (weight_sum / weight_factors.pow(2).sum())) * 100
-                report['Weighting efficiency'] = efficiency                
-                report['Iterations required'] = group['iterations']                
-                report['Mean weight factor'] = minimum = filtered_data[weight_col].mean()
-                report['Minimum weight factor'] = minimum = filtered_data[weight_col].min()
-                report['Maximum weight factor'] = maximum = filtered_data[weight_col].max()                
+                report['Total: weighted'] = weight_sum
+                report['Weighting efficiency'] = efficiency
+                report['Iterations required'] = group['iterations']
+                report['Mean weight factor'] = minimum = mean
+                report['Minimum weight factor'] = minimum = minimum
+                report['Maximum weight factor'] = maximum = maximum
                 report['Weight factor ratio'] = maximum / minimum
-                
+
                 reports.append(report)
 
-        report_df = pd.DataFrame(reports).set_index(['Weight variable', 'Weight group', 'Weight filter']).T
+        report_df = pd.DataFrame(reports).set_index([
+            'Weight variable',
+            'Weight group',
+            'Weight filter'
+        ]).T
         return report_df
 
     def run(self, schemes=[]):
