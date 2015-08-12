2103e6749f4fe269b31afec67f677c7bb707819c
Alasdair Eaglestone <AlasdairE@users.noreply.github.com>
2015-08-12 14:37:58 +0100

    y-keys with a width of 1 will now be formatted like y-keys with a width greater than 1, i.e. with a title and category label, rather than as a single merged cell like the 'Total' column.

--- core/builds/excel/excel_painter.py
+++ core/builds/excel/excel_painter.py
@@ -337,9 +337,14 @@ def write_column_labels(worksheet, labels, existing_format, row,
     try:
         if levels == 0:
             worksheet.set_column(cols[0], cols[1], 10)
-            worksheet.merge_range(
-                row, cols[0], row, cols[1], labels[0][0], existing_format
-            )
+            if cols[0] == cols[1]:
+                worksheet.write_row(
+                    row, cols[0], labels[0],  existing_format
+                )
+            else:
+                worksheet.merge_range(
+                    row, cols[0], row, cols[1], labels[0][0], existing_format
+                )
             worksheet.write_row(row+1, cols[0], labels[1],  existing_format)
         elif levels > 0:
             worksheet.set_column(cols[0], cols[1], 10)
@@ -1226,7 +1231,7 @@ def ExcelPainter(path_excel,
                         #write y labels - NESTING WORKING FOR 2 LEVELS. NEEDS TO WORK FOR N LEVELS.
                         y_name = 'Total' if y_name == '@' else y_name
 
-                        if df_cols[idx][0] == df_cols[idx][1]:
+                        if y_name == 'Total':
                             if coordmap['x'][x_name][fullname][0] == ROW_INDEX_ORIGIN+(nest_levels*2)+bool(testcol_maps):
                                 #write column label(s) - multi-column y subaxis
                                 worksheet.set_column(
