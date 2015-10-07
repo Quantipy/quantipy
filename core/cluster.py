25dbbed1a68f77cb25112cfe4e62bb2fe64c9523
James Griffiths <jamesrkg@users.noreply.github.com>
2015-10-07 14:43:18 -0400

    Added support for dict-based banked chain definitions in the Cluster.

--- core/cluster.py
+++ core/cluster.py
@@ -22,17 +22,65 @@ class Cluster(OrderedDict):
     def __reduce__(self):
         return self.__class__, (self.name, ), self.__dict__, None, self.iteritems()
 
+    def _verify_banked_chain_definition(chain):
+        """
+        Verify chain conforms to the expected banked chain structure.
+        """
+        if not isinstance(chain, dict):
+            return False
+        try:
+            ctype = chain['type']
+            cname = chain['name']
+            ctext = chain['text']
+            citems = chain['items']
+            for c in citems:
+                cichain = c['chain']
+                citext = c['text']
+        except:
+            return False
+
+        if not all([isinstance(c, Chain) for c in citems['chain']]):
+            return False
+
+        if not all([isinstance(t, dict) for t in citems['text']]):
+            return False
+
+        if not isinstance(cviews, list):
+            return False
+
+        cview = chain.get('view', None)
+        if view is None:
+            for c in citems:
+                if not 'view' in c:
+                    return False
+        else:
+            if not isinstance(cview, str)):
+                return False
+
+        return True
+
     def add_chain(self, chains=None):
         """ Adds chains to a cluster """
         # If a single item was supplied, change it to a list of items
-        if not isinstance(chains, (list, Chain, pd.DataFrame)):
-            raise TypeError("You must pass either a Chain or a list of Chains to Cluster.add_chain()")
+        if not isinstance(chains, (list, Chain, pd.DataFrame, dict)):
+            raise TypeError(
+                "You must pass either a Chain, a list of Chains or a"
+                " banked chain definition (as a dict) into"
+                " Cluster.add_chain().")
+        if isinstance(chains, dict):
+            if not self._verify_banked_chain_definition(chains):
+                raise TypeError(
+                    "Your banked-chain definition is not correctly"
+                    " formed. Please check it again.")
         else:
 
             if isinstance(chains, Chain):
                 self[chains.name] = chains
 
-            elif isinstance(chains, list) and all([isinstance(chain, Chain) for chain in chains]):
+            elif isinstance(chains, list) and all([
+                isinstance(chain, Chain) or \
+                self.verify_banked_chain_definition(chain) 
+                for chain in chains]):
                 # Ensure that all items in chains is of the type Chain.
                 for chain in chains:
                     self[chain.name] = chain
@@ -43,6 +91,38 @@ class Cluster(OrderedDict):
             else:
                 # One or more of the items in chains is not a chain.
                 raise TypeError("One or more of the supplied chains has an inappropriate type.")
+
+    def bank_chains(self, spec):
+
+        dk = chains[0].data_key
+        fk = chains[0].filter
+        yks = chains[0].content_of_axis
+        
+        if not isinstance(vks, (list, tuple)):
+            vks = [vks]
+        
+        banked = {}
+        for yk in yks:
+            banked[yk] = []
+            for chain in chains:
+                xk = chain.source_name
+                banked[yk].append(
+                    pd.concat([
+                        get_dataframe(chain, keys=[dk, fk, xk, yk, vk])
+                        for vk in vks],
+                        axis=0)
+                    )
+            banked[yk] = pd.concat(banked[yk], axis=0)
+            if banked[yk].columns.levels[1][0]=='@':
+                banked[yk] = pd.DataFrame(
+                    banked[yk].max(axis=1),
+                    index=banked[yk].index,
+                    columns=pd.MultiIndex.from_tuples(
+                        [(chains[0].source_name, '@')],
+                        names=['Question', 'Values'])
+                )
+        
+        return banked        
 
     def _build(self, type):
         """ The Build exports the chains using methods supplied with 'type'. """
