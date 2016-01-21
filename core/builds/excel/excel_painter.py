9e6852c49b0942ec0d7ca4ca17f077ab68ed3956
James Griffiths <jamesrkg@users.noreply.github.com>
2016-01-20 23:54:06 -0500

    Forced cell details for array summary sheets to be simple Row Percentages.

--- core/builds/excel/excel_painter.py
+++ core/builds/excel/excel_painter.py
@@ -1959,11 +1959,14 @@ def ExcelPainter(path_excel,
             #Add cell contents to end of sheet
             if len(cell_details)>0:
                 if is_array:
+                    cell_details = 'Cell Contents (Row Percentages)'
                     r = end_x + 3
+                    worksheet.write_string(
+                        row=r, col=1, string=cell_details, cell_format=formats['x_right'])
                 else:
                     r = current_position['x'] + 1
-                worksheet.write_string(
-                    row=r, col=1, string=cell_details, cell_format=formats['x_right'])
+                    worksheet.write_string(
+                        row=r, col=1, string=cell_details, cell_format=formats['x_right'])
 
             #set column widths
             worksheet.set_column(col_index_origin-1, col_index_origin-1, 40)
