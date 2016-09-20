f5b90d7a3ad52957115c7c398a13804db2165816
Alexander Tanski <alexander.tanski@yougov.de>
2016-09-20 16:38:00 +0200

    adding some standalone know_views for stddev, min, max

--- core/view_generators/view_maps.py
+++ core/view_generators/view_maps.py
@@ -118,6 +118,30 @@ class QuantipyViews(ViewMapper):
                 'text': ''
             }
         }
+        self.known_methods['stddev'] = {
+            'method': 'descriptives',
+            'kwargs': {
+                'stats': 'stddev',
+                'axis': 'x',
+                'text': ''
+            }
+        }
+        self.known_methods['min'] = {
+            'method': 'descriptives',
+            'kwargs': {
+                'stats': 'min',
+                'axis': 'x',
+                'text': ''
+            }
+        }
+        self.known_methods['max'] = {
+            'method': 'descriptives',
+            'kwargs': {
+                'stats': 'max',
+                'axis': 'x',
+                'text': ''
+            }
+        }
 
     def default(self, link, name, kwargs):
         """
