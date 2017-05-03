a55b1238f02d69ad267e480f50fd11c3d9279241
Alexander Tanski <alexander.tanski@yougov.de>
2017-05-03 13:37:56 +0200

    bugfixing cum_sum calculation in frequency view method

--- core/view_generators/view_maps.py
+++ core/view_generators/view_maps.py
@@ -318,6 +318,7 @@ class QuantipyViews(ViewMapper):
             else:
                 raw = True if name in ['counts_sum', 'c%_sum'] else False
                 cum_sum = True if name in ['counts_cumsum', 'c%_cumsum'] else False
+                if cum_sum: axis = None
                 q.count(axis=axis, raw_sum=raw, as_df=False, margin=False, cum_sum=cum_sum)
             if rel_to is not None:
                 if q.type == 'array':
