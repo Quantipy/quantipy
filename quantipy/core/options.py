5e2c62d3ecd02b703fbf4fc7cc1e47eeca4a212d
AlasdairE <alasdair.eaglestone@gmail.com>
2017-08-02 18:36:20 +0100

    Moving options.py to new core location

---
+++ quantipy/core/options.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+OPTIONS = {
+	'new_rules': False,
+	'new_chains': False,
+	'short_item_texts': False
+}
+
+def set_option(option, val):
+	"""
+	"""
+	if not option in OPTIONS:
+		err = "'{}' is not a valid option!".format(option)
+		raise ValueError(err)
+	OPTIONS[option] = val
+	return None
+
