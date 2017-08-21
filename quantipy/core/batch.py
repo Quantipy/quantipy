bb88fda2b32ad1868f9a3a3b13c4a016476a14b2
YG\KMue <kerstin.mueller@yougov.de>
2017-08-21 15:52:28 +0200

    batch, extend x and y

--- quantipy/core/batch.py
+++ quantipy/core/batch.py
@@ -342,6 +342,39 @@ class Batch(qp.DataSet):
         self.make_summaries(masks)
         return None
 
+    @modify(to_list=['ext_xks'])
+    def extend_x(self, ext_xks):
+        """
+        Extend downbreak variables with additional variables.
+
+        Parameters
+        ----------
+        ext_xks: str/ dict, list of str/dict
+            Name(s) of variable(s) that are added as downbreak. If a dict is
+            provided, the variable is added in front of the belonging key.
+            Example::
+            >>> ext_xks = ['var1', {'existing_x': ['var2', 'var3']}]
+
+            var1 is added at the end of the downbreaks, var2 and var3 are
+            added in front of the variable existing_x.
+
+        Returns
+        -------
+            None
+        """
+        for x in ext_xks:
+            if isinstance(x, dict):
+                for pos, var in x.items():
+                    if not isinstance(var, list): var = [var]
+                    for v in var:
+                        if not v in self.xks:
+                            self.xks.insert(self.xks.index(pos), v)
+            elif x not in self.xks:
+                self.xks.append(x)
+        self._update()
+        return None
+
+
     @modify(to_list='arrays')
     @verify(variables={'arrays': 'masks'})
     def make_summaries(self, arrays):
@@ -535,15 +568,20 @@ class Batch(qp.DataSet):
         return None
 
     @modify(to_list=['ext_yks', 'on'])
-    @verify(variables={'ext_yks': 'both', 'on': 'both'})
     def extend_y(self, ext_yks, on=None):
         """
         Add y (crossbreak/banner) variables to specific x (downbreak) variables.
 
         Parameters
         ----------
-        ext_yks: str/ list of str
-            Name(s) of variable(s) that are added as crossbreak.
+        ext_yks: str/ dict, list of str/ dict
+            Name(s) of variable(s) that are added as crossbreak. If a dict is
+            provided, the variable is added in front of the beloning key.
+            Example::
+            >>> ext_yks = ['var1', {'existing_y': ['var2', 'var3']}]
+
+            var1 is added at the end of the crossbreaks, var2 and var3 are
+            added in front of the variable existing_y.
         on: str/ list of str
             Name(s) of variable(s) in the xks (downbreaks) for which the
             crossbreak should be extended.
@@ -654,12 +692,26 @@ class Batch(qp.DataSet):
         -------
         None
         """
+        def _order_yks(yks):
+            y_keys = []
+            for y in yks:
+                if isinstance(y, dict):
+                    for pos, var in y.items():
+                        if not isinstance(var, list): var = [var]
+                        for v in var:
+                            if not v in y_keys:
+                                y_keys.insert(y_keys.index(pos), v)
+                elif not y in y_keys:
+                    y_keys.append(y)
+            return y_keys
+
         def _extend(x, mapping):
             mapping[x] = org_copy.deepcopy(self.yks)
             if x in self.extended_yks_per_x:
                 mapping[x].extend(self.extended_yks_per_x[x])
             if x in self.exclusive_yks_per_x:
                 mapping[x] = self.exclusive_yks_per_x[x]
+            mapping[x] = _order_yks(mapping[x])
 
         mapping = OrderedDict()
         for x in self.xks:
