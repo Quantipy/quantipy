b3d7e075053993976411aea492055a2b0c609414
YG\KMue <kerstin.mueller@yougov.de>
2017-09-19 09:30:03 +0200

    differ between arrays and columns vars with multiple groups

--- quantipy/core/builds/powerpoint/transformations.py
+++ quantipy/core/builds/powerpoint/transformations.py
@@ -403,7 +403,7 @@ def validate_cluster_orientations(cluster):
 '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
 
-def get_base(df, base_description):
+def get_base(df, base_description, is_mask):
     '''
     Constructs base text for any kind of chart, single, multiple, grid.
 
@@ -450,11 +450,12 @@ def get_base(df, base_description):
         # if all_same(base_values[0]):
         #     base_text = base_text_format(base_description, base_values[0][0])
         # else:
-        # it = zip(top_members, base_values[0])
-        # base_texts = ', '.join([base_text_format(x, y) for x, y in it])
-        # base_text = ' - '.join([base_description, base_texts])
-        base_text = base_text_format(base_description, base_values[0][0])
-
+        if not is_mask:
+            it = zip(top_members, base_values[0])
+            base_texts = ', '.join([base_text_format(x, y) for x, y in it])
+            base_text = ' - '.join([base_description, base_texts])
+        else:
+            base_text = base_text_format(base_description, base_values[0][0])
     return base_text
 
 
