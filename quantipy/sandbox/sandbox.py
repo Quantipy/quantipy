0b0ef6a63b41c9251178e93b2659124503430304
Alexander Buchhammer <alexander.buchhammer@yougov.de>
2017-10-26 10:29:12 +0200

    small change for the wip from_cmt() return

--- quantipy/sandbox/sandbox.py
+++ quantipy/sandbox/sandbox.py
@@ -176,10 +176,10 @@ class ChainManager(object):
         self.source = 'Crunch multitable'
         cubegroups = ctb_per_cubegroup(crunch_tabbook, ignore=ignore)
         chain_dataframes = cubegroups_to_cdf(cubegroups)
-        # self.__chains = the_chains_from_the_cmt
-        return chain_dataframes
+        self.__chains = chain_dataframes
+
         # return None
-        #  return self
+        return self
 
 
     def from_cluster(self, clusters):
