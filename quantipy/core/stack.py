c21621dbb7209f20cf61d43f85dddc4eb9deab0a
YG\KMue <kerstin.mueller@yougov.de>
2017-12-12 11:57:45 +0100

    aggregate unwgt bases if no weight is provided

--- quantipy/core/stack.py
+++ quantipy/core/stack.py
@@ -1889,8 +1889,9 @@ class Stack(defaultdict):
                         # add bases
                         for ba, weights in new_bases.items():
                             if weights.get('wgt'):
-                                self.add_link(dk, f, x=x, y=y, views=[ba], weights=w)
-                            if weights.get('unwgt') and not None in w:
+                                ba_w = [b_w for b_w in w if not b_w is None]
+                                self.add_link(dk, f, x=x, y=y, views=[ba], weights=ba_w)
+                            if weights.get('unwgt'):
                                 if not (x in v_typ['array'] or any(yks in v_typ['array'] for yks in y)):
                                     self.add_link(dk, f, x=x, y=y, views=[ba], weights=None)
                         # remove existing nets for link if new view is a net
