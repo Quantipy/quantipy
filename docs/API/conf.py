9af005639529a4bd6c2607b4b60169e649dee806
YG\KMue <kerstin.mueller@yougov.de>
2018-09-21 14:37:15 +0200

    docs

--- docs/API/conf.py
+++ docs/API/conf.py
@@ -19,6 +19,7 @@ import os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, "C:\\Users\\alt\\AppData\\Local\\Continuum\\Anaconda\\Lib\\site-packages\\quantipy")
+sys.path.insert(0, "C:\\Users\\kmue\\Desktop\\quantipy")
 
 # -- General configuration ------------------------------------------------
 
