42a069183d313aafe125bc8bf471dfe5f475620a
Alexander Buchhammer <alt@YOUGOV.local>
2018-11-05 08:00:00 +0100

    docs

--- quantipy/sandbox/sandbox.py
+++ quantipy/sandbox/sandbox.py
@@ -2706,8 +2706,7 @@ class Chain(object):
                 x_frames.append(pd.concat(y_frames, axis=concat_axis))
 
                 self.shapes.append(x_frames[-1].shape)
-
-            self._frame = pd.concat(self._pad(x_frames), axis=self.axis)
+                self._frame = pd.concat(self._pad(x_frames), axis=self.axis)
 
             if self._group_style == 'reduced' and self.array_style >- 1:
                 if not any(len(v) == 2 and any(view.split('|')[1].startswith('t.')
