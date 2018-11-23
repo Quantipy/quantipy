447ec11470c708ecc3aa66a7b9bfafd788a234f4
Alexander Buchhammer <alt@YOUGOV.local>
2018-11-23 18:10:16 +0100

    testing: how to modify excel.py to stop alternate_bg for sig level rows (INDIA)

--- quantipy/sandbox/excel.py
+++ quantipy/sandbox/excel.py
@@ -1121,7 +1121,6 @@ class _Box(object):
             level_1, values, contents = (levels(1).values,
                                          self.values,
                                          self.contents)
-
         row_max = max(contents.keys())
         flat = np.c_[level_1.T, values].flat
         rel_x, rel_y = flat.coords
@@ -1150,7 +1149,6 @@ class _Box(object):
                 name = 'counts'
             else:
                 name = self._row_format_name(**x_contents)
-
             if rel_y == 0:
                 if data == '':
                     view_border = False
@@ -1264,6 +1262,7 @@ class _Box(object):
         if ((is_freq_test and not_net_sum) or freq_view_group) or \
                 (not is_mean and self.chain.array_style == 0):
             return not bg, bg
+
         return self.sheet.alternate_bg, True
 
     def _row_format_name(self, **contents):
@@ -1390,7 +1389,8 @@ class _Box(object):
         while True:
             try:
                 ndx, next_ = next(it)
-                if next_ == '':
+                sig_level_row = self.contents[ndx]['siglevel'] and next_ != ''
+                if next_ == '' or sig_level_row:
                     if not group:
                         group = data
                     if self._is('test', **self.contents[ndx]):
