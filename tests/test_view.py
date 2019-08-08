91a4a5f78972303f7a2b17c24bf5262cc97b6fa6
Kerstin Müller <kerstin.mueller@yougov.de>
2019-08-08 19:23:35 +0200

    fake View test

--- tests/test_view.py
+++ tests/test_view.py
@@ -1,16 +1,13 @@
-import unittest
-import pandas as pd
-import json
-from copy import deepcopy
-from quantipy.core.stack import Stack
-from quantipy.core.view import View
-from quantipy.core.helpers.functions import load_json
 
-class TestLinkResult(unittest.TestCase):
+from quantipy.core.view import View
+
+
+class TestView:
     # The View object has undergone massive changes.
     # We will need to add tests for all new View attributes and methods
     # that are used make self-inspection possible.
     # Also: the constructor is changend completely.
-    pass
 
-
+    def test_view_fake(self):
+        view = View()
+        assert isinstance(view, View)
